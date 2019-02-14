df8adfbf8ca23a3909baa7c87c48d86c230a6b05
Tolendi10 <47203652+Tolendi10@users.noreply.github.com>
2019-02-14 12:12:06 +0600

    Add files via upload

---
+++ Task 2/Task 2/Task 2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Student
+    {
+        public string name;
+        public string id;
+        public string year;
+
+        public Student()
+        {
+            name = Console.ReadLine();
+            id = Console.ReadLine();
+            year = Console.ReadLine();
+        }
+
+        public void PrintInfo()
+        {
+            Console.Write("Student: ");
+            Console.WriteLine(name);
+            Console.Write("Student`s id number: ");
+            Console.WriteLine(id);
+            Console.Write("Student`s year of study: ");
+            Console.WriteLine(year);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.PrintInfo();
+        }
+    }
+}
